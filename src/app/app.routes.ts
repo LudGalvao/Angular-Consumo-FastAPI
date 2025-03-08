d4a5fb4c1f1e550a8bcff4f42b69b329c271ce93
LudGalvao <ludgalvao09@gmail.com>
2025-03-08 16:52:51 -0300

    ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BuscaComponent } from './components/busca/busca.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: BuscaComponent }
+];
