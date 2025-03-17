223bf611f16f5a46065c190c60abf29af75334e8
LudGalvao <ludgalvao09@gmail.com>
2025-03-17 10:37:49 -0300

    ts

--- src/app/components/busca/busca.component.ts
+++ src/app/components/busca/busca.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-busca',
-  imports: [InputTextModule],
+  imports: [InputTextModule, ButtonModule],
   templateUrl: './busca.component.html',
   styleUrl: './busca.component.css'
 })
