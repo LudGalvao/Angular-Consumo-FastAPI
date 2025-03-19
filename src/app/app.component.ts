c471e004431fe97938c1fc633887f85539bfef51
LudGalvao <ludgalvao09@gmail.com>
2025-03-19 15:38:30 -0300

    ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BuscaComponent } from './components/busca/busca.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BuscaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
